6556a991fb45235d30337e3ca9796c6ffa28c1ce
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2017-12-21 16:03:53 -0500

    A lot of changes

--- game/js/oldcode.js
+++ game/js/oldcode.js
@@ -3,7 +3,8 @@
 var game = new Phaser.Game(256, 240,  Phaser.AUTO, '', {
   preload: preload,
   create: create,
-  update: update
+  update: update,
+  render: render
 });
 
 game.state.add('game', game);
@@ -16,9 +17,12 @@ function preload() {
 // Images
   game.load.image('simples_pimples', 'assets/images/simples_pimples.png');
   game.load.image('paper', 'assets/images/paper.png');
+  game.load.image('spike', 'assets/images/spike.png');
+  game.load.image('background', 'assets/images/cityscape.png');
 
 // Spritesheets
   game.load.spritesheet('dude', 'assets/images/SP_player_spritesheet.png', 16, 16);
+  game.load.spritesheet('bat', 'assets/images/bat.png', 16, 16);
 
 // Audio
   game.load.audio('music', 'assets/audio/music.mp3');
@@ -30,13 +34,18 @@ function preload() {
 var map;
 var tileset;
 var layer;
+var bg;
 
 // Player vars
 var player;
 var controls;
+var jumpButton;
 
 // Enemy vars
 var enemy;
+var spike;
+var bat;
+
 // SFX vars
 var music;
 var jumpSound;
@@ -46,11 +55,14 @@ var papers;
 var score = 0;
 var scoreText;
 
+
 function create() {
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   game.stage.backgroundColor = '#000000';
+  // bg = game.add.tileSprite(0, 0, 1000, 600, 'background');
+  // bg.fixedToCamera = true;
 
   map = game.add.tilemap('level1');
 
@@ -68,11 +80,22 @@ function create() {
   // The player and its settings
   player = game.add.sprite(32, game.world.height - 150, 'dude');
 
+  bat = game.add.sprite(48, game.world.height - 100, 'bat');
+
   //  We need to enable physics on the player
   game.physics.arcade.enable(player, Phaser.Physics.ARCADE);
+  game.physics.arcade.enable(bat, Phaser.Physics.ARCADE);
 
-  //  Player physics properties. Give the little guy a slight bounce.
+  // Removes bat if out of world bounds
+  bat.checkWorldBounds = true;
+  bat.outOfBoundsKill = true;
+
+  bat.body.velocity.x = -10;
+
+
+  //  Player physics properties.
   player.body.bounce.y = 0.1;
+  player.body.gravity.y = 1250;
   player.body.gravity.y = 1500;
   // player.body.collideWorldBounds = true;
 
@@ -80,7 +103,28 @@ function create() {
   player.animations.add('left', [0, 1], 10, true);
   player.animations.add('right', [4, 5], 10, true);
 
+  bat.animations.add('fly-left', [0, 1], 5, true);
+  bat.animations.add('right', [2, 3], 10, true);
+  bat.play('fly-left');
+  bat.body.moves = false;
+
+  bat.enableBody = true;
+
+  // bat.body.gravity.y = 300;
+
   game.camera.follow(player);
+
+  // Spikes
+  spike = [game.add.sprite(416, 112, 'spike'),
+          game.add.sprite(432, 112, 'spike'),
+          game.add.sprite(448, 112, 'spike'),
+          game.add.sprite(480, 112, 'spike'),
+          game.add.sprite(496, 112, 'spike'),
+          game.add.sprite(528, 112, 'spike'),
+          game.add.sprite(544, 112, 'spike')];
+
+  spike.enablebody = true;
+  game.physics.arcade.enable(spike, Phaser.Physics.ARCADE);
 
   //  Finally some papers to collect
   papers = game.add.group();
@@ -110,6 +154,7 @@ function create() {
 
   //  Our controls.
   controls = game.input.keyboard.createCursorKeys();
+  jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
   // Audio
   music = game.add.audio('music');
@@ -120,7 +165,7 @@ function create() {
 
   game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-  game.input.onDown.add(gofull, this);
+  game.input.onDown.add(gofull);
 }
 
 function update() {
@@ -128,9 +173,12 @@ function update() {
   //  Collide the player and the papers with the platforms
   game.physics.arcade.collide(player, layer);
   game.physics.arcade.collide(papers, layer);
+  game.physics.arcade.collide(bat, layer);
 
   //  Checks to see if the player overlaps with any of the papers, if he does call the collectPaper function
   game.physics.arcade.overlap(player, papers, collectPaper, null, this);
+  game.physics.arcade.overlap(player, spike, playerDie, null, this);
+  // game.physics.arcade.overlap(player, bat, playerDie, null, this);
 
   //  Reset the players velocity (movement)
   player.body.velocity.x = 0;
@@ -149,11 +197,11 @@ function update() {
     //  Stand still
     player.animations.stop();
 
-    player.frame = 3;
+    // player.frame = 3;
   }
 
   //  Allow the player to jump if they are touching the ground.
-  if (controls.up.isDown && player.body.onFloor()) {
+  if (jumpButton.isDown && player.body.onFloor()) {
     player.body.velocity.y = -350;
     jumpSound.play();
 
@@ -162,6 +210,13 @@ function update() {
   if (!player.inWorld) {
     playerDie();
 }
+
+}
+
+function render() {
+
+    // Sprite debug info
+    // game.debug.spriteInfo(player, 32, 32);
 
 }
 
@@ -196,7 +251,6 @@ function gofull() {
 function playerDie() {
   score = 0;
   music.stop();
-
   game.state.start(game.state.current);
 
 

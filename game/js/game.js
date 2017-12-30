afe79809aa97769efa93e23b31b1f0bd52b25ec0
Devin Hart <devohart@gmail.com>
2017-12-30 11:34:16 -0500

    All enemy functionality added

--- game/js/game.js
+++ game/js/game.js
@@ -17,16 +17,20 @@ function preload() {
   game.load.tilemap('level1', 'assets/levels/level1_map_final.json', null, Phaser.Tilemap.TILED_JSON);
 
 // Images
+  game.load.image('background', 'assets/images/bg_grad.png');
+  game.load.image('invisWall', 'assets/images/invisible_wall.png');
   game.load.image('simples_pimples', 'assets/images/simples_pimples.png');
   game.load.image('spikeDown', 'assets/images/spike_down.png');
   game.load.image('spikeUp', 'assets/images/spike_up.png');
-  game.load.image('background', 'assets/images/bg_grad.png');
-  game.load.image('invisWall', 'assets/images/invisible_wall.png');
 
 // Spritesheets
   game.load.spritesheet('player', 'assets/images/skeletal_player.png', 16, 16);
   game.load.spritesheet('bat', 'assets/images/bat.png', 16, 16);
+  game.load.spritesheet('devil', 'assets/images/devil.png', 16, 16);
   game.load.spritesheet('ghost', 'assets/images/ghost.png', 16, 16);
+  game.load.spritesheet('goblin', 'assets/images/goblin.png', 16, 16);
+  game.load.spritesheet('redSkeleton', 'assets/images/red_skeleton.png', 16, 16);
+  game.load.spritesheet('yellowSkeleton', 'assets/images/yellow_skeleton.png', 16, 16);
   game.load.spritesheet('zombie', 'assets/images/zombie.png', 16, 16);
 
 // Audio
@@ -48,8 +52,13 @@ var jumpButton;
 
 // Enemy vars
 var spikeDown;
+var spikeUp;
 var batGroup;
+var devilGroup;
+var goblinGroup;
 var ghostGroup;
+var redSkeletonGroup;
+var yellowSkeletonGroup;
 var zombieGroup;
 
 // SFX vars
@@ -74,7 +83,8 @@ function create() {
   layer = map.createLayer('Tile Layer 1');
   layer.resizeWorld();
 
-  map.setCollisionByExclusion([0, 108, 203, 253, 803, 1257, 1307, 1357, 1407, 1759]);
+  map.setCollisionByExclusion([0, 108, 201, 203, 251, 253, 403, 803, 1257, 1307,
+    453, 1357, 1407, 1759]);
 // Collision debug
   // layer.debug = true;
 
@@ -99,6 +109,21 @@ function create() {
   batGroup.callAll('animations.add', 'animations', 'bat-left', [0, 1], 5, true);
   batGroup.callAll('animations.play', 'animations', 'bat-left');
 
+// Devil info
+  devilGroup = game.add.physicsGroup();
+  game.physics.arcade.enable(devilGroup, Phaser.Physics.ARCADE);
+  devilGroup.enableBody = true;
+  devilGroup.checkWorldBounds = true;
+  devilGroup.outOfBoundsKill = true;
+  // devilGroup.create(1120, 208, 'devil');
+  devilGroup.callAll('animations.add', 'animations', 'devil-left', [0, 1, 2], 5, true);
+  devilGroup.callAll('animations.add', 'animations', 'devil-right', [3, 4, 5], 5, true);
+
+  devilGroup.forEach(function(devilGroup) {
+    devilGroup.body.velocity.x = -10;
+    devilGroup.body.bounce.x = 1;
+    devilGroup.body.gravity.y = 1500;
+  });
 // Ghost info
   ghostGroup = game.add.physicsGroup();
   game.physics.arcade.enable(ghostGroup, Phaser.Physics.ARCADE);
@@ -111,6 +136,55 @@ function create() {
 
   ghostGroup.forEach(function(ghostGroup) {
     ghostGroup.body.velocity.x = -25;
+  });
+
+// Goblin info
+  goblinGroup = game.add.physicsGroup();
+  game.physics.arcade.enable(goblinGroup, Phaser.Physics.ARCADE);
+  goblinGroup.enableBody = true;
+  goblinGroup.checkWorldBounds = true;
+  goblinGroup.outOfBoundsKill = true;
+  // goblinGroup.create(1120, 208, 'goblin');
+  goblinGroup.callAll('animations.add', 'animations', 'goblin-left', [0, 1, 2], 5, true);
+  goblinGroup.callAll('animations.add', 'animations', 'goblin-right', [3, 4, 5], 5, true);
+
+  goblinGroup.forEach(function(goblinGroup) {
+    goblinGroup.body.velocity.x = -10;
+    goblinGroup.body.bounce.x = 1;
+    goblinGroup.body.gravity.y = 1500;
+});
+
+// Red Skeleton Group
+  redSkeletonGroup = game.add.physicsGroup();
+  game.physics.arcade.enable(redSkeletonGroup, Phaser.Physics.ARCADE);
+  redSkeletonGroup.enableBody = true;
+  redSkeletonGroup.checkWorldBounds = true;
+  redSkeletonGroup.outOfBoundsKill = true;
+  // redSkeletonGroup.create(1120, 208, 'redSkeleton');
+  redSkeletonGroup.callAll('animations.add', 'animations', 'redSkeleton-left', [0, 1, 2], 5, true);
+  redSkeletonGroup.callAll('animations.add', 'animations', 'redSkeleton-right', [3, 4, 5], 5, true);
+
+  redSkeletonGroup.forEach(function(redSkeletonGroup) {
+    redSkeletonGroup.body.velocity.x = -10;
+    redSkeletonGroup.body.bounce.x = 1;
+    redSkeletonGroup.body.gravity.y = 1500;
+  });
+
+// Yellow Skeleton info
+
+  yellowSkeletonGroup = game.add.physicsGroup();
+  game.physics.arcade.enable(yellowSkeletonGroup, Phaser.Physics.ARCADE);
+  yellowSkeletonGroup.enableBody = true;
+  yellowSkeletonGroup.checkWorldBounds = true;
+  yellowSkeletonGroup.outOfBoundsKill = true;
+  // yellowSkeletonGroup.create(1120, 208, 'yellowSkeleton');
+  yellowSkeletonGroup.callAll('animations.add', 'animations', 'yellowSkeleton-left', [0, 1, 2], 5, true);
+  yellowSkeletonGroup.callAll('animations.add', 'animations', 'yellowSkeleton-right', [3, 4, 5], 5, true);
+
+  yellowSkeletonGroup.forEach(function(yellowSkeletonGroup) {
+    yellowSkeletonGroup.body.velocity.x = -10;
+    yellowSkeletonGroup.body.bounce.x = 1;
+    yellowSkeletonGroup.body.gravity.y = 1500;
   });
 
 // Zombie info
@@ -129,7 +203,7 @@ function create() {
     zombieGroup.body.gravity.y = 1500;
   });
 
-// Spike info
+// Spike Down info
   spikeDown = [
     game.add.sprite(416, 112, 'spikeDown'),
     game.add.sprite(432, 112, 'spikeDown'),
@@ -142,6 +216,12 @@ function create() {
 
   spikeDown.enablebody = true;
   game.physics.arcade.enable(spikeDown, Phaser.Physics.ARCADE);
+
+// Spike Up info
+  spikeUp = [];
+
+  spikeUp.enablebody = true;
+  game.physics.arcade.enable(spikeUp, Phaser.Physics.ARCADE);
 
 // Invisible walls info
   invisWall = game.add.group();
@@ -191,14 +271,33 @@ function update() {
 
 // Asset collision
   game.physics.arcade.collide(player, layer);
+
   game.physics.arcade.collide(batGroup, layer);
+  game.physics.arcade.collide(batGroup, invisWall);
+
+  game.physics.arcade.collide(devilGroup, layer);
+  game.physics.arcade.collide(devilGroup, invisWall);
+
+  game.physics.arcade.collide(goblinGroup, layer);
+  game.physics.arcade.collide(goblinGroup, invisWall);
+
+  game.physics.arcade.collide(ghostGroup, invisWall);
+
+  game.physics.arcade.collide(redSkeletonGroup, layer);
+  game.physics.arcade.collide(redSkeletonGroup, invisWall);
+
+  game.physics.arcade.collide(yellowSkeletonGroup, layer);
+  game.physics.arcade.collide(yellowSkeletonGroup, invisWall);
+
   game.physics.arcade.collide(zombieGroup, layer);
   game.physics.arcade.collide(zombieGroup, invisWall);
+
   // game.physics.arcade.collide(papers, layer);
 
 // Overlap functions
-  game.physics.arcade.overlap(player, spikeDown, playerDie, null, this);
   game.physics.arcade.overlap(player, ghostGroup, playerDie, null, this);
+  game.physics.arcade.overlap(player, spikeDown, playerDie, null, this);
+  game.physics.arcade.overlap(player, spikeUp, playerDie, null, this);
   // game.physics.arcade.overlap(player, papers, collectPaper, null, this);
 
 // Reset the players velocity (movement)
@@ -229,6 +328,7 @@ function update() {
     playerDie();
   }
 
+// Kill enemy on jump (bat, goblin, red and yellow skeleton, zombie)
   game.physics.arcade.collide(player, batGroup, function(player, batGroup) {
     if (batGroup.body.touching.up && player.body.touching.down) {
       player.body.velocity.y = -300;
@@ -240,12 +340,89 @@ function update() {
 
   // , null, this ^ if broken insert between } & );
 
+  game.physics.arcade.collide(player, goblinGroup, function(player, goblinGroup) {
+    if (goblinGroup.body.touching.up && player.body.touching.down) {
+      player.body.velocity.y = -300;
+      goblinGroup.kill();
+    } else {
+      playerDie();
+    }
+  });
+
+  game.physics.arcade.collide(player, redSkeletonGroup, function(player, redSkeletonGroup) {
+    if (redSkeletonGroup.body.touching.up && player.body.touching.down) {
+      player.body.velocity.y = -300;
+      redSkeletonGroup.kill();
+    } else {
+      playerDie();
+    }
+  });
+
+  game.physics.arcade.collide(player, yellowSkeletonGroup, function(player, yellowSkeletonGroup) {
+    if (yellowSkeletonGroup.body.touching.up && player.body.touching.down) {
+      player.body.velocity.y = -300;
+      yellowSkeletonGroup.kill();
+    } else {
+      playerDie();
+    }
+  });
+
   game.physics.arcade.collide(player, zombieGroup, function(player, zombieGroup) {
     if (zombieGroup.body.touching.up && player.body.touching.down) {
       player.body.velocity.y = -300;
       zombieGroup.kill();
     } else {
       playerDie();
+    }
+  });
+
+// Render left and right animation
+
+  batGroup.forEach(function(batGroup) {
+    if (batGroup.body.velocity.x > 0) {
+      batGroup.animations.play('bat-right');
+    } else {
+      batGroup.animations.play('bat-left');
+    }
+  });
+
+  devilGroup.forEach(function(devilGroup) {
+    if (devilGroup.body.velocity.x > 0) {
+      devilGroup.animations.play('devil-right');
+    } else {
+      devilGroup.animations.play('devil-left');
+    }
+  });
+
+  ghostGroup.forEach(function(ghostGroup) {
+    if (ghostGroup.body.velocity.x > 0) {
+      ghostGroup.animations.play('ghost-right');
+    } else {
+      ghostGroup.animations.play('ghost-left');
+    }
+  });
+
+  goblinGroup.forEach(function(goblinGroup) {
+    if (goblinGroup.body.velocity.x > 0) {
+      goblinGroup.animations.play('goblin-right');
+    } else {
+      goblinGroup.animations.play('goblin-left');
+    }
+  });
+
+  redSkeletonGroup.forEach(function(redSkeletonGroup) {
+    if (redSkeletonGroup.body.velocity.x > 0) {
+      redSkeletonGroup.animations.play('redSkeleton-right');
+    } else {
+      redSkeletonGroup.animations.play('redSkeleton-left');
+    }
+  });
+
+  yellowSkeletonGroup.forEach(function(yellowSkeletonGroup) {
+    if (yellowSkeletonGroup.body.velocity.x > 0) {
+      yellowSkeletonGroup.animations.play('yellowSkeleton-right');
+    } else {
+      yellowSkeletonGroup.animations.play('yellowSkeleton-left');
     }
   });
 
